20c5b46a5738e526ba9afac236a94b9d92d8dc64
BenRaz123 <ben.raz2008@gmail.com>
2024-01-18 10:07:47 -0500

    init
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hex Playground
+
+## What is this?
+
+This is a website where you can enter hex codes and see the background update _instantly_!
+
+## Why
+
+I did not see any good alternatives
---
+++ index.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("input");
+const bg = document.getElementsByTagName("body")[0];
+
+const randomHexColor = () => {
+    let r = randint(256);
+    let g = randint(256);
+    let b = randint(256);
+
+    r = r > 16 ? r.toString(16) : `0${r.toString(16)}`;
+    g = g > 16 ? g.toString(16) : `0${g.toString(16)}`;
+    b = b > 16 ? b.toString(16) : `0${b.toString(16)}`;
+
+    return `${r}${g}${b}`;
+}
+
+const randint = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+const handler = (e) => {
+    const val = e.target.value;
+    const width = e.target.value.length + 1;
+    document.body.style.backgroundColor = `#${val}`;
+    input.style.width = `${width}ch`;
+}
+
+const startingColor = randomHexColor(); 
+
+input.setAttribute("placeholder", startingColor);
+document.body.style.backgroundColor = `#${startingColor}`;
+
+input.addEventListener('input', handler);
